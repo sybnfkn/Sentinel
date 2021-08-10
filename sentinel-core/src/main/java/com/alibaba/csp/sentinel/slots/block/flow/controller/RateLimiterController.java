5f021900f8cbe748d89d162fa0f3fc2430acb074
sybnfkn <18322695691@163.com>
2021-08-10 10:33:19 +0800

    注释

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/flow/controller/RateLimiterController.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/flow/controller/RateLimiterController.java
@@ -24,12 +24,16 @@ import com.alibaba.csp.sentinel.node.Node;
 
 /**
  * @author jialiang.linjl
+ * 排队等待
  */
 public class RateLimiterController implements TrafficShapingController {
 
+    // 排队最大时长，默认 500ms
     private final int maxQueueingTimeMs;
+    // QPS 设置的值
     private final double count;
 
+    // 记录上一次请求通过的时间
     private final AtomicLong latestPassedTime = new AtomicLong(-1);
 
     public RateLimiterController(int timeOut, double count) {
@@ -42,6 +46,7 @@ public class RateLimiterController implements TrafficShapingController {
         return canPass(node, acquireCount, false);
     }
 
+    // 通常 acquireCount 为 1，这里不用关心参数 prioritized
     @Override
     public boolean canPass(Node node, int acquireCount, boolean prioritized) {
         // Pass when acquire count is less or equal than 0.
@@ -56,29 +61,38 @@ public class RateLimiterController implements TrafficShapingController {
 
         long currentTime = TimeUtil.currentTimeMillis();
         // Calculate the interval between every two requests.
+        // 计算每 2 个请求之间的间隔，比如 QPS 限制为 10，那么间隔就是 100ms
         long costTime = Math.round(1.0 * (acquireCount) / count * 1000);
 
         // Expected pass time of this request.
         long expectedTime = costTime + latestPassedTime.get();
 
+        // 可以通过，设置 latestPassedTime 然后就返回 true 了
         if (expectedTime <= currentTime) {
             // Contention may exist here, but it's okay.
+            // 好久都没流量过来了，直接返回通过，时间设置当前时间
             latestPassedTime.set(currentTime);
             return true;
         } else {
+            // 不可以通过，需要等待
             // Calculate the time to wait.
             long waitTime = costTime + latestPassedTime.get() - TimeUtil.currentTimeMillis();
+            // 等待时长大于最大值，返回 false。别等时间太长了，不然上游都超时啦！
             if (waitTime > maxQueueingTimeMs) {
                 return false;
             } else {
+                // 将 latestPassedTime 往前推
                 long oldTime = latestPassedTime.addAndGet(costTime);
                 try {
+                    // 需要 sleep 的时间
                     waitTime = oldTime - TimeUtil.currentTimeMillis();
                     if (waitTime > maxQueueingTimeMs) {
                         latestPassedTime.addAndGet(-costTime);
                         return false;
                     }
                     // in race condition waitTime may <= 0
+                    // 时间不扣回去了，就这么占着，然后等待固定时常
+                    // 不拒绝，缓冲一会，可以将流量进行整形
                     if (waitTime > 0) {
                         Thread.sleep(waitTime);
                     }
